e525d6bbfe48b7826f05e778b59cfdaa98232106
somelijer <116906162+somelijer@users.noreply.github.com>
2024-03-19 18:45:55 +0100

    feat: reroute available eqipment call

--- src/Explorer.API/Controllers/Author/Administration/EquipmentController.cs
+++ src/Explorer.API/Controllers/Author/Administration/EquipmentController.cs
@@ -2,6 +2,8 @@
 using Explorer.Tours.API.Public.Administration;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace Explorer.API.Controllers.Author.Administration
 {
@@ -9,18 +11,52 @@ namespace Explorer.API.Controllers.Author.Administration
     [Route("api/manipulation/equipment")]
     public class EquipmentController : BaseApiController
     {
-        private readonly IEquipmentService _equipmentService;
+        private string microserviceUrl = "http://localhost:8081";
+        private HttpClient _httpClient = new HttpClient();
 
         public EquipmentController(IEquipmentService equipmentService)
         {
-            _equipmentService = equipmentService;
+
         }
 
         [HttpPost("get-available/{tourId:int}")]
-        public ActionResult<List<EquipmentDto>> GetAvailableEquipment([FromBody]List<long> currentEquipmentIds, int tourId)
+        public async Task<ActionResult<List<EquipmentDto>>> GetAvailableEquipment([FromBody] List<long> currentEquipmentIds, int tourId)
         {
-            var result = _equipmentService.GetAvailable(currentEquipmentIds, tourId);
-            return CreateResponse(result);
+            try
+            {
+                // Serialize the currentEquipmentIds into JSON
+                var jsonContent = JsonConvert.SerializeObject(currentEquipmentIds);
+
+                // Create HTTP request content with JSON content
+                var httpContent = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+                // Send HTTP POST request to the microservice
+                var response = await _httpClient.PostAsync($"{microserviceUrl}/equipments/available/{tourId}", httpContent);
+
+                // Check if the request was successful
+                if (response.IsSuccessStatusCode)
+                {
+                    // Deserialize the response content to EquipmentDto list
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    var availableEquipment = JsonConvert.DeserializeObject<List<EquipmentDto>>(responseContent);
+
+                    // Return the list of available equipment
+                    return Ok(availableEquipment);
+                }
+                else
+                {
+                    // If the response is unsuccessful, return appropriate error status
+                    Console.WriteLine($"HTTP request failed with status code {response.StatusCode}");
+                    return StatusCode((int)response.StatusCode);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle and log any exceptions that occur
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                return StatusCode(500);
+            }
         }
     }
+
 }
